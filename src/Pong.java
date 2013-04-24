04bcb0fa1bb7a407a37768f6556d768af306b22e
Malcolm <mkane827@gmail.com>
2013-04-24 13:13:33 -0500

    Creating the project

---
+++ src/Pong.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Malcolm
+ * Date: 4/24/13
+ * Time: 1:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Pong {
+}
