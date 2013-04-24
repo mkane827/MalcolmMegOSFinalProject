1844d3b3e45561247c6c5d0fb2334a13e41cf495
Malcolm <mkane827@gmail.com>
2013-04-24 13:30:56 -0500

    Creating the project

--- src/Pong.java
+++ src/Pong.java
@@ -1,3 +1,8 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+import java.awt.event.MouseEvent;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Malcolm
@@ -5,5 +10,35 @@
  * Time: 1:10 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Pong {
+public class Pong extends GraphicsProgram {
+
+    private int screenWidth = 500;
+    private int screenHeight = 500;
+
+    public MouseEvent mouseEvent;
+
+    public void run() {
+
+    }
+
+    public void init() {
+        setSize(screenWidth, screenHeight);
+        super.init();
+    }
+
+    public int getScreenWidth() {
+        return screenWidth;
+    }
+
+    public void setScreenWidth(int screenWidth) {
+        this.screenWidth = screenWidth;
+    }
+
+    public int getScreenHeight() {
+        return screenHeight;
+    }
+
+    public void setScreenHeight(int screenHeight) {
+        this.screenHeight = screenHeight;
+    }
 }
