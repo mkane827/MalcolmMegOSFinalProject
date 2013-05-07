53d2bf1b9f906054e7fd5b200001c58bd13aef2e
meggrose <mnaminski@gmail.com>
2013-05-06 21:12:22 -0500

    More organization and game logic

---
+++ src/ServerGame.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Meg Naminski
+ * Date: May 6, 2013
+ * Time: 3:52:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerGame {
+}
---
+++ src/UserGame.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Meg Naminski
+ * Date: May 6, 2013
+ * Time: 3:52:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserGame {
+}
